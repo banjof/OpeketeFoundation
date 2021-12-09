a09edf4f3b7c0d25eaa33f2c0d8377bb0be44df2
banjof <coolfob@yahoo.com>
2021-12-09 06:46:42 +0100

    Update index.js

--- OpeketeFoundation/opekete/src/main-component/router/index.js
+++ OpeketeFoundation/opekete/src/main-component/router/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Homepage from '../HomePage'
 import AboutPage from '../AboutPage'
